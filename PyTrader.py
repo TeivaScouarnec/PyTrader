1505e882c3ba6246cb00fa18d2a89cfbef3318bd
Teiva <64995222+TeivaScouarnec@users.noreply.github.com>
2022-05-17 16:47:10 +0200

    PyTrader => add SellOrder()

--- PyTrader.py
+++ PyTrader.py
@@ -1,13 +1,9 @@
-from email import header
-from time import gmtime, strftime
-import pandas
 import yfinance as yf;
 from pandas_datareader import data as pd
 import urllib3
 import json
 
 http = urllib3.PoolManager()
-
 
 f = open("user.json","+r")
 Account=json.load(f)
@@ -16,19 +12,42 @@ f.close
 user = Account['user']
 key = Account['key']
 authorization = Account['authorization']
+headerRequest = {
+    "Authorization": f"Bearer {authorization}",
+    'Content-Type': 'application/json'
+}
 
-
-def getCurrentAction(enterment : str):
-    data = yf.download(enterment, group_by="column",period='1d')
+def getCurrentAction(company : str):
+    data = yf.download(company, group_by="column",period='1d')
     currentAction = data['Close'].values[0]
     float(currentAction)
     return currentAction
 
-def getOrdersAccount():
-    url1= f"https://streaming.saxotrader.com/sim/openapi/streaming/connection?authorization=BEARER{authorization}&contextID=explorer_1652715200460"
-    r=http.request('GET',url1)
-    print(r.status)
+def getAllOrdersAccount():
+    urlSaxo= f"https://gateway.saxobank.com/sim/openapi/port/v1/orders/me"
+    r=http.request('GET',urlSaxo, headers=headerRequest)
+    if (r.status == 200):
+        return (json.loads(r.data))
+    else:
+        return (str("null"))
+
+def sellOrder():
+    urlSaxo= f"https://gateway.saxobank.com/sim/openapi/trade/v2/orders"
+    datarOrder = json.dumps({
+        "Uic": 21,
+        "AssetType": "FxSpot",
+        "Amount": 10000,
+        "BuySell": "Buy",
+        "OrderType": "Market",
+        "AmountType": "Quantity",
+        "ManualOrder": False,
+        "OrderDuration": {
+            "DurationType": "DayOrder"
+        }
+    })
+    r=http.request('POST',urlSaxo, headers= headerRequest, body= datarOrder)
+    print (r.status)
     return r.data
     
 print (getCurrentAction("RI.PA"))
-print (getOrdersAccount())
+print (getAllOrdersAccount())
